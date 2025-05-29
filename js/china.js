aceb668a363a3b3a9262e743eb3f0b4757b08012
fontanillus <arcamapa@gmail.com>
2025-05-29 10:21:44 +0200

    Add files via upload

--- js/china.js
+++ js/china.js
@@ -12,17 +12,31 @@ function calcular() {
     let yearNac = parseInt(YEAR.value);
     let mesNac = parseInt(MES.value);
 
-    if (isNaN(yearNac) || yearNac < 1900 || yearNac > new Date().getFullYear()) {
+    if (
+        isNaN(yearNac) ||
+        yearNac < 1900 ||
+        yearNac > new Date().getFullYear() ||
+        isNaN(mesNac) ||
+        mesNac < 1 ||
+        mesNac > 12
+    ) {
+        SIGNO.innerHTML = "";
         IMGSIGNO.src = "";
         return;
     }
 
-    let indice = yearNac % 12;
-    let signoChino = simbolos[indice];
-   
+    // Si nació en enero, restamos un año (por el calendario chino)
+    if (mesNac == 1) {
+        yearNac = yearNac - 1;
+    }
 
+    // Cálculo del signo chino
+    const signoIndex = (yearNac - 1900) % 12;
+    const signoChino = simbolos[signoIndex];
+
+    // Mostrar resultado
     SIGNO.innerHTML = `Tu signo del zodíaco es <b>${signoChino}</b>`;
-    imagenSigno = `./img/img${signoChino}.jpg`;
+    const imagenSigno = `./img/img${signoChino}.jpg`;
     IMGSIGNO.src = imagenSigno;
     IMGSIGNO.style.display = 'block';
     IMGSIGNO.style.margin = '0 auto';
